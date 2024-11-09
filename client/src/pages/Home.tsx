dcc4e0da54c65b517544bc3294f0ecd7f0098e26
Yash Rajput <02yashrajput@gmail.com>
2024-11-09 19:05:05 +0530

    commit

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,15 @@
+import useSWR from 'swr';
+import axios from 'axios';
 
-
+const fetcher = (url:string) => axios.get(url).then(res => res.data);
 const Home = () => {
-
+  // const { data, error } = useSWR('/api/data-endpoint', fetcher);
+  
   return (
-    <div>Home</div>
+    <div>
+      
+
+    </div>
   )
 }
 
