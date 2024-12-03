73d09f45bf6d34118c9b0e70419b58c7685d3a4a
ujjwallsrivastavaa <2003ujjwalusri@gmail.comm>
2024-12-04 02:03:20 +0530

    ongoing and tablecomponent lang updated

--- client/src/components/ContractList/TableComponent.tsx
+++ client/src/components/ContractList/TableComponent.tsx
@@ -11,6 +11,7 @@ import {
   Typography,
 } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next"; 
 
 interface TableComponentProps {
   contracts: {
@@ -41,6 +42,7 @@ interface TableComponentProps {
 }
 
 const TableComponent: React.FC<TableComponentProps> = ({ contracts }) => {
+  const { t } = useTranslation(["tablecomponent", "crops"]);
   return (
     <TableContainer
       component={Paper}
@@ -50,25 +52,25 @@ const TableComponent: React.FC<TableComponentProps> = ({ contracts }) => {
         <TableHead>
           <TableRow>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Farmer</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Farmer")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Buyer</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Buyer")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Initial Payment</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Initial Payment")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Final Payment</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Final Payment")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Delivery Status</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Delivery Status")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Deadline</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Deadline")}</Typography>
             </TableCell>
             <TableCell>
-              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>Product</Typography>
+              <Typography variant="h6" sx={{fontSize:15 , fontWeight:600}}>{t("Product")}</Typography>
             </TableCell>
           </TableRow>
         </TableHead>
