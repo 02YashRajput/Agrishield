a8c41b0e6ffe5661aa5232a38266533510282167
Yash Rajput <02yashrajput@gmail.com>
2024-12-11 01:00:59 +0530

    added pm2 and ready to deploy

---
+++ server/ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  apps: [
+    {
+      name: 'agrishield',
+      script: './src/index.mjs',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      watch: true, 
+      autorestart: true, 
+      restart_delay: 1000,
+      error_file: './logs/app-err.log',
+      out_file: './logs/app-out.log', 
+      log_date_format: 'DD-MM-YYYY HH:mm:ss', 
+    },
+  ],
+};
