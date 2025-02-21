476e686b5c26d9aeb2a7ae80b4f3757200d44b43
Yash Rajput <02yashrajput@gmail.com>
2025-02-21 12:15:18 +0530

    commit

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-   
-      '/api': {
-        target: 'http://localhost:5001', 
-       
-       
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_SERVER_URL || 'http://localhost:5001',
+          changeOrigin: true,
+          secure: true, 
+        },
       },
     },
-  },
+  }
 })
